403ffbecb1a45464449c19a0990931b000170b65
evandrofra <evandrofra@gmail.com>
2022-11-11 14:56:28 -0300

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,9 @@
+opcao=0
+while opcao<3:
+    print('Escolha a opca')
+    print('1-Teste')
+    print('2-Teste 2')
+    print('3-Sair')
+    opcao=int(input(opcao))
+    print('Voce escolheu', opcao)
+print('Saiu')
