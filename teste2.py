286f30399b7eb7f3f4a4c56e24811937c09024bb
evandrofra <evandrofra@gmail.com>
2022-11-11 15:45:09 -0300

    Create teste2.py

---
+++ teste2.py
@@ -0,0 +1,16 @@
+#import teste as te
+#b=2
+#print (te.x(b))
+#a=15
+#print (te.fteste(a))
+
+opcao=0
+while opcao<3:
+    print('Escolha sua opcao:')
+    print('1 - Cadastro clientes')
+    print('2 - Cadastro de veiculos')
+    print('3 - Sair')
+    opcao = int(input())
+    print('Voce escolheu', opcao)
+
+print('Aplicacao finalizada')
